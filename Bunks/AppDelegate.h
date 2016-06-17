f4d3fdd220d22375c3707533e92ed248fc771c9c
Jayden Wan <jaydenwan@jaydens-mac.local>
2016-06-17 10:31:26 +0800

    Initial Commit

---
+++ Bunks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bunks
+//
+//  Created by Jayden Wan on 6/17/16.
+//  Copyright © 2016 Hua Wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bunks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bunks
+//
+//  Created by Jayden Wan on 6/17/16.
+//  Copyright © 2016 Hua Wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
